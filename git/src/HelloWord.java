270015f7353915ba9cd4800010e3f7323042eb2f
wfx <1019773958@qq.com>
2019-07-12 20:20:44 +0800

    添加到本地仓库

---
+++ git/src/HelloWord.java
@@ -0,0 +1,8 @@
+public class HelloWord {
+
+    public static void main(String[] args) {
+        System.out.println("asfdaf");
+        System.out.println("你好");
+        System.out.println("中国");
+    }
+}
