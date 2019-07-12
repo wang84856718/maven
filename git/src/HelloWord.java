c882444b97be68f9989ccf9f7c817a8436c17224
wfx <1019773958@qq.com>
2019-07-12 21:04:02 +0800

    添加到本地仓库

--- git/src/HelloWord.java
+++ git/src/HelloWord.java
@@ -5,6 +5,8 @@ public class HelloWord {
         System.out.println("你好");
         System.out.println("中国");
         System.out.println("长城");
+        System.out.println("斗宗强者恐怖如斯");
+        System.out.println("what are you doing");
     }
 
     public void show(){
